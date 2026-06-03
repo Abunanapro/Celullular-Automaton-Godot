d405953df4f3096b060d0de7c9f5d18d655873cb
Abunanapro <arnaupro27@gmail.com>
2026-06-03 14:41:19 +0200

    1.3
    Optimized the proccesed pixels list so now performance is x2  times better, fixed steam behaviour now it checks processed list like other elements. Also I added a icon to the project

--- Scripts/sandsystem.gd
+++ Scripts/sandsystem.gd
@@ -12,8 +12,11 @@ var holding= false
 #Stat Vars
 var SandUpdates = 0
 var WaterUpdates = 0
+var SteamUpdates = 0
 # Called when the node enters the scene tree for the first time.
-
+#Important paths
+@onready var tilemap = $TileMapLayer
+@onready var timer = $Timer
 #Interface Path Vars
 #labels
 #game stat labels
@@ -36,38 +39,38 @@ var WaterUpdates = 0
 @onready var canvaslineedit=$"../CanvasLayer/UI/MarginContainer3/HBoxContainer/canvas"
 @onready var timelineedit=$"../CanvasLayer/UI/MarginContainer3/HBoxContainer/time"
 
-
+@onready var updates=0
 func _ready() -> void:
-	$Timer.timeout.connect(_on_timer_timeout) #this makes shure the signal is conneted ti timer
-	$Timer.start()   #starts timer just in case it is not starting for some weird reason...
-
+	timer.timeout.connect(_on_timer_timeout) #this makes shure the signal is conneted ti timer
+	timer.start()   #starts timer just in case it is not starting for some weird reason...
+	
 var last_canvas_text = ""
 var last_time_text=""
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-
-	
-	
 	fpslabel.text=("FPS: "+str(Engine.get_frames_per_second()))
 	SandCountLabel.text=("Sand: "+str(SandUpdates))
 	WaterCountLabel.text=("Water: "+str(WaterUpdates))
+	SteamCountLabel.text=("Steam: "+str(SteamUpdates))
 	if canvaslineedit.text != last_canvas_text:
 		last_canvas_text = canvaslineedit.text
 		var scale_val = float(canvaslineedit.text) / 10.0
-		$TileMapLayer.scale = Vector2(scale_val, scale_val)
+		tilemap.scale = Vector2(scale_val, scale_val)
 	if timelineedit.text!=last_time_text:
 		last_time_text=timelineedit.text
-		$Timer.wait_time=0.1/float(timelineedit.text)
-		$Timer.start()
+		timer.wait_time=0.1/float(timelineedit.text)
+		timer.start()
 	if $"../CanvasLayer/UI/MarginContainer3/HBoxContainer/clear".button_pressed==true:
-		$TileMapLayer.clear()
+		tilemap.clear()
 	if holding:
 		var mouse_pos = get_viewport().get_mouse_position()
-		var tilexy = $TileMapLayer.local_to_map($TileMapLayer.to_local(mouse_pos))
-		$TileMapLayer.set_cell(tilexy, brush, Vector2i(0, 0))
+		var tilexy = tilemap.local_to_map(tilemap.to_local(mouse_pos))
+		var offset = floor(brush_size / 2.0)
+		tilemap.set_cell(tilexy, brush, Vector2i(0, 0))
 		for n in brush_size:
 			for i in brush_size:
-				$TileMapLayer.set_cell(tilexy+Vector2i(n,i), brush, Vector2i(0, 0))
+				var pos = tilexy + Vector2i(n - offset, i - offset)
+				tilemap.set_cell(pos, brush, Vector2i(0, 0))
 		
 func updatebrush():
 	brush_size=int(brushlineedit.text)
@@ -87,29 +90,34 @@ func _input(event:InputEvent) -> void:
 	
 
 	if  event is InputEventMouseButton:
-		var tilexy=$TileMapLayer.local_to_map($TileMapLayer.to_local(event.position))
+		var tilexy=tilemap.local_to_map(tilemap.to_local(event.position))
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			holding = event.pressed
 			updatebrush()
-			$TileMapLayer.set_cell(tilexy,brush,Vector2i(0,0))
+			tilemap.set_cell(tilexy,brush,Vector2i(0,0))
 		if event.button_index == MOUSE_BUTTON_RIGHT:
 			holding = event.pressed
-			$TileMapLayer.set_cell(tilexy,brush,Vector2i(-1,-1))
+			tilemap.set_cell(tilexy,brush,Vector2i(-1,-1))
 		
 		
 
 func _on_timer_timeout() -> void:
-	var ProccesedCells:Array
+	
+	updateslabel.text=("Active pixels: "+str(updates))
+	updates=0
+	var ProccesedCells = {}
 	var cells:Array
 	#automatically calculates active pixel ammount
 	for x in range(ActiveMaterialList.size()):
-		cells =cells + $TileMapLayer.get_used_cells_by_id(ActiveMaterialList[x])
-	#cells = $TileMapLayer.get_used_cells_by_id(1)+$TileMapLayer.get_used_cells_by_id(2)+$TileMapLayer.get_used_cells_by_id(4)
-	var updates=0
+		cells =cells + tilemap.get_used_cells_by_id(ActiveMaterialList[x])
+	#cells = tilemap.get_used_cells_by_id(1)+tilemap.get_used_cells_by_id(2)+tilemap.get_used_cells_by_id(4)
+	
+	
 	SandUpdates=0
 	WaterUpdates=0
+	SteamUpdates=0
 	for cell in cells:
-		var cellIndex = $TileMapLayer.get_cell_source_id(cell)
+		var cellIndex = tilemap.get_cell_source_id(cell)
 		updates=updates+1
 		var up = cell + Vector2i(0, -1)
 		var down = cell + Vector2i(0, 1)
@@ -117,111 +125,134 @@ func _on_timer_timeout() -> void:
 		var left_bottom = cell + Vector2i(-1, 1)
 		var left = cell + Vector2i(-1, 0)   
 		var right = cell + Vector2i(1, 0) 
-		match cellIndex:
-			ID_SAND:
-				#Update counter
-				SandUpdates=SandUpdates+1
-				#Sand Information
-				var ID=ID_SAND
-				var density=DensityList[ID_SAND-1]
-				
-				if $TileMapLayer.get_cell_source_id(down)==-1 and not(down in ProccesedCells) and not(cell in ProccesedCells):
-					$TileMapLayer.set_cell(cell,-1)
-					$TileMapLayer.set_cell(down, ID,Vector2i(0, 0))
-					ProccesedCells.append(cell)
-					ProccesedCells.append(down)
-				else:
-					if DensityList[$TileMapLayer.get_cell_source_id(down)-1]!=null:
-						if DensityList[$TileMapLayer.get_cell_source_id(down)-1]<density and not(down in ProccesedCells) and not(cell in ProccesedCells):
-							$TileMapLayer.set_cell(cell,$TileMapLayer.get_cell_source_id(down),Vector2i(0, 0))
-							$TileMapLayer.set_cell(down, ID,Vector2i(0, 0))
-							ProccesedCells.append(cell)
-							ProccesedCells.append(down)
+		
+		if not ProccesedCells.has(cell):
+			match cellIndex:
+				ID_SAND:
+					#Update counter
+					SandUpdates=SandUpdates+1
+					#Sand Information
+					var ID=ID_SAND
+					var density=DensityList[ID_SAND-1]
+					
+					if tilemap.get_cell_source_id(down)==-1 and not ProccesedCells.has(down) and not ProccesedCells.has(cell):
+						tilemap.set_cell(cell,-1)
+						tilemap.set_cell(down, ID,Vector2i(0, 0))
+						ProccesedCells[cell] = true
+						ProccesedCells[down] = true
+					else:
+						if DensityList[tilemap.get_cell_source_id(down)-1]!=null:
+							if DensityList[tilemap.get_cell_source_id(down)-1]<density and not ProccesedCells.has(down) and not ProccesedCells.has(cell):
+								tilemap.set_cell(cell,tilemap.get_cell_source_id(down),Vector2i(0, 0))
+								tilemap.set_cell(down, ID,Vector2i(0, 0))
+								ProccesedCells[cell] = true
+								ProccesedCells[down] = true
+								
+						if randi_range(0,1) ==1:
+							if tilemap.get_cell_source_id(left_bottom)==-1 and tilemap.get_cell_source_id(left)==-1 and not ProccesedCells.has(left_bottom) and not ProccesedCells.has(cell):
+								tilemap.set_cell(cell,-1)
+								tilemap.set_cell(left_bottom, ID,Vector2i(0, 0))
+								ProccesedCells[cell] = true
+								ProccesedCells[left_bottom] = true
+							elif DensityList[tilemap.get_cell_source_id(left)-1]!=null and DensityList[tilemap.get_cell_source_id(left_bottom)-1]!=null:
+								if DensityList[tilemap.get_cell_source_id(left_bottom)-1]<density and DensityList[tilemap.get_cell_source_id(left)-1]<density and not ProccesedCells.has(cell) and not ProccesedCells.has(left_bottom):
+									tilemap.set_cell(cell,tilemap.get_cell_source_id(left_bottom),Vector2i(0, 0))
+									tilemap.set_cell(left_bottom, ID,Vector2i(0, 0))
+									ProccesedCells[cell] = true
+									ProccesedCells[left_bottom] = true
+						else:
+							if tilemap.get_cell_source_id(right_bottom)==-1 and tilemap.get_cell_source_id(right)==-1 and not ProccesedCells.has(cell) and not ProccesedCells.has(right_bottom):
+								tilemap.set_cell(cell,-1)
+								tilemap.set_cell(right_bottom, ID,Vector2i(0, 0))
+								ProccesedCells[cell] = true
+								ProccesedCells[right_bottom] = true
 							
-					if randi_range(1,0) ==1:
-						if $TileMapLayer.get_cell_source_id(left_bottom)==-1 and $TileMapLayer.get_cell_source_id(left)==-1 and not(left_bottom in ProccesedCells) and not(cell in ProccesedCells):
-							$TileMapLayer.set_cell(cell,-1)
-							$TileMapLayer.set_cell(left_bottom, ID,Vector2i(0, 0))
-							ProccesedCells.append(cell)
-							ProccesedCells.append(left_bottom)
-						elif DensityList[$TileMapLayer.get_cell_source_id(left)-1]!=null and DensityList[$TileMapLayer.get_cell_source_id(left_bottom)-1]!=null:
-							if DensityList[$TileMapLayer.get_cell_source_id(left_bottom)-1]<density and DensityList[$TileMapLayer.get_cell_source_id(left)-1]<density and (not(cell in ProccesedCells) and not(left_bottom in ProccesedCells)):
-								$TileMapLayer.set_cell(cell,$TileMapLayer.get_cell_source_id(left_bottom),Vector2i(0, 0))
-								$TileMapLayer.set_cell(left_bottom, ID,Vector2i(0, 0))
-								ProccesedCells.append(cell)
-								ProccesedCells.append(left_bottom)
-					else:
-						if $TileMapLayer.get_cell_source_id(right_bottom)==-1 and $TileMapLayer.get_cell_source_id(right)==-1 and (not(cell in ProccesedCells) and not(right_bottom in ProccesedCells)):
-							$TileMapLayer.set_cell(cell,-1)
-							$TileMapLayer.set_cell(right_bottom, ID,Vector2i(0, 0))
-							ProccesedCells.append(cell)
-							ProccesedCells.append(right_bottom)
-						
-						elif DensityList[$TileMapLayer.get_cell_source_id(right)-1]!=null and DensityList[$TileMapLayer.get_cell_source_id(right_bottom)-1]!=null:
-							if DensityList[$TileMapLayer.get_cell_source_id(right_bottom)-1]<density and DensityList[$TileMapLayer.get_cell_source_id(right)-1]<density and (not(cell in ProccesedCells) and not(right_bottom in ProccesedCells)):
-								$TileMapLayer.set_cell(cell,$TileMapLayer.get_cell_source_id(right_bottom),Vector2i(0, 0))
-								$TileMapLayer.set_cell(right_bottom, ID,Vector2i(0, 0))
-								ProccesedCells.append(cell)
-								ProccesedCells.append(right_bottom)
-			ID_WATER:
-				var ID=ID_WATER
-				WaterUpdates=WaterUpdates+1
-				var density=DensityList[ID_WATER-1]
-			
-				if $TileMapLayer.get_cell_source_id(down)==-1 and not(down in ProccesedCells) and not(cell in ProccesedCells):
-					$TileMapLayer.set_cell(cell,-1)
-					$TileMapLayer.set_cell(down, ID_WATER,Vector2i(0, 0))
-					ProccesedCells.append(cell)
-					ProccesedCells.append(down)
-				else:
-					if DensityList[$TileMapLayer.get_cell_source_id(down)-1]!=null:
-						if DensityList[$TileMapLayer.get_cell_source_id(down)-1]<density and not(down in ProccesedCells) and not(cell in ProccesedCells):
-							$TileMapLayer.set_cell(cell,$TileMapLayer.get_cell_source_id(down),Vector2i(0, 0))
-							$TileMapLayer.set_cell(down, ID,Vector2i(0, 0))
-							ProccesedCells.append(cell)
-							ProccesedCells.append(down)
-					if randi_range(1,0) ==1:
-						if $TileMapLayer.get_cell_source_id(left)==-1 and (not(cell in ProccesedCells) or not(left in ProccesedCells)):
-							$TileMapLayer.set_cell(cell,-1)
-							$TileMapLayer.set_cell(left, ID,Vector2i(0, 0))
-							ProccesedCells.append(cell)
-							ProccesedCells.append(left)
-						elif DensityList[$TileMapLayer.get_cell_source_id(left)-1]!=null:
-							if DensityList[$TileMapLayer.get_cell_source_id(left)-1]<density  and (not(cell in ProccesedCells) or not(left in ProccesedCells)):
-								$TileMapLayer.set_cell(cell,$TileMapLayer.get_cell_source_id(left),Vector2i(0, 0))
-								$TileMapLayer.set_cell(left, ID,Vector2i(0, 0))
-								ProccesedCells.append(cell)
-								ProccesedCells.append(left)
-					else:
-						if $TileMapLayer.get_cell_source_id(right)==-1 and (not(cell in ProccesedCells) or not(right in ProccesedCells)):
-							$TileMapLayer.set_cell(cell,-1)
-							$TileMapLayer.set_cell(right, ID,Vector2i(0, 0))
-							ProccesedCells.append(cell)
-							ProccesedCells.append(right)
-						elif DensityList[$TileMapLayer.get_cell_source_id(right)-1]!=null:
-							if DensityList[$TileMapLayer.get_cell_source_id(right)-1]<density  and (not(cell in ProccesedCells) or not(right in ProccesedCells)):
-								$TileMapLayer.set_cell(cell,$TileMapLayer.get_cell_source_id(right),Vector2i(0, 0))
-								$TileMapLayer.set_cell(right, ID,Vector2i(0, 0))
-								ProccesedCells.append(cell)
-								ProccesedCells.append(right)
-			ID_STEAM:
-				var density=DensityList[ID_STEAM-1]
+							elif DensityList[tilemap.get_cell_source_id(right)-1]!=null and DensityList[tilemap.get_cell_source_id(right_bottom)-1]!=null:
+								if DensityList[tilemap.get_cell_source_id(right_bottom)-1]<density and DensityList[tilemap.get_cell_source_id(right)-1]<density and not ProccesedCells.has(cell) and not ProccesedCells.has(right_bottom):
+									tilemap.set_cell(cell,tilemap.get_cell_source_id(right_bottom),Vector2i(0, 0))
+									tilemap.set_cell(right_bottom, ID,Vector2i(0, 0))
+									ProccesedCells[cell] = true
+									ProccesedCells[right_bottom] = true
+				ID_WATER:
+					var ID=ID_WATER
+					WaterUpdates=WaterUpdates+1
+					var density=DensityList[ID_WATER-1]
 				
-			
-				if $TileMapLayer.get_cell_source_id(up)==-1:
-					$TileMapLayer.set_cell(cell,-1)
-					$TileMapLayer.set_cell(up, ID_STEAM,Vector2i(0, 0))
-				else:
-					if DensityList[$TileMapLayer.get_cell_source_id(up)-1]!=null:
-						if DensityList[$TileMapLayer.get_cell_source_id(up)-1]<density:
-							$TileMapLayer.set_cell(cell,$TileMapLayer.get_cell_source_id(up),Vector2i(0, 0))
-							$TileMapLayer.set_cell(up, ID_STEAM,Vector2i(0, 0))
-					if randi_range(1,0) ==1:
-						if $TileMapLayer.get_cell_source_id(left)==-1:
-							$TileMapLayer.set_cell(cell,-1)
-							$TileMapLayer.set_cell(left, ID_STEAM,Vector2i(0, 0))
+					if tilemap.get_cell_source_id(down)==-1 and not ProccesedCells.has(cell) and not ProccesedCells.has(down):
+						tilemap.set_cell(cell,-1)
+						tilemap.set_cell(down, ID,Vector2i(0, 0))
+						ProccesedCells[cell] = true
+						ProccesedCells[down] = true
 					else:
-						if $TileMapLayer.get_cell_source_id(right)==-1:
-							$TileMapLayer.set_cell(cell,-1)
-							$TileMapLayer.set_cell(right, ID_STEAM,Vector2i(0, 0))
-	updateslabel.text=("Active pixels: "+str(updates))
+						if DensityList[tilemap.get_cell_source_id(down)-1]!=null:
+							if DensityList[tilemap.get_cell_source_id(down)-1]<density and not ProccesedCells.has(cell) and not ProccesedCells.has(down):
+								tilemap.set_cell(cell,tilemap.get_cell_source_id(down),Vector2i(0, 0))
+								tilemap.set_cell(down, ID,Vector2i(0, 0))
+								ProccesedCells[cell] = true
+								ProccesedCells[down] = true
+						if randi_range(1,0) ==1:
+							if tilemap.get_cell_source_id(left)==-1 and not ProccesedCells.has(cell) and not ProccesedCells.has(left):
+								tilemap.set_cell(cell,-1)
+								tilemap.set_cell(left, ID,Vector2i(0, 0))
+								ProccesedCells[cell] = true
+								ProccesedCells[left] = true
+							elif DensityList[tilemap.get_cell_source_id(left)-1]!=null:
+								if DensityList[tilemap.get_cell_source_id(left)-1]<density  and not ProccesedCells.has(cell) and not ProccesedCells.has(left):
+									tilemap.set_cell(cell,tilemap.get_cell_source_id(left),Vector2i(0, 0))
+									tilemap.set_cell(left, ID,Vector2i(0, 0))
+									ProccesedCells[cell] = true
+									ProccesedCells[left] = true
+						else:
+							if tilemap.get_cell_source_id(right)==-1 and not ProccesedCells.has(cell) and not ProccesedCells.has(right):
+								tilemap.set_cell(cell,-1)
+								tilemap.set_cell(right, ID,Vector2i(0, 0))
+								ProccesedCells[cell] = true
+								ProccesedCells[right] = true
+							elif DensityList[tilemap.get_cell_source_id(right)-1]!=null:
+								if DensityList[tilemap.get_cell_source_id(right)-1]<density  and not ProccesedCells.has(cell) and not ProccesedCells.has(right):
+									tilemap.set_cell(cell,tilemap.get_cell_source_id(right),Vector2i(0, 0))
+									tilemap.set_cell(right, ID,Vector2i(0, 0))
+									ProccesedCells[cell] = true
+									ProccesedCells[right] = true
+				ID_STEAM:
+					var density=DensityList[ID_STEAM-1]
+					var ID=ID_STEAM
+					SteamUpdates=SteamUpdates+1
+					if tilemap.get_cell_source_id(up)==-1 and not ProccesedCells.has(cell) and not ProccesedCells.has(up):
+						tilemap.set_cell(cell,-1)
+						tilemap.set_cell(up, ID,Vector2i(0, 0))
+						ProccesedCells[cell] = true
+						ProccesedCells[up] = true
+					else:
+						if DensityList[tilemap.get_cell_source_id(up)-1]!=null:
+							if DensityList[tilemap.get_cell_source_id(up)-1]<density and not ProccesedCells.has(cell) and not ProccesedCells.has(up):
+								tilemap.set_cell(cell,tilemap.get_cell_source_id(up),Vector2i(0, 0))
+								tilemap.set_cell(up, ID,Vector2i(0, 0))
+								ProccesedCells[cell] = true
+								ProccesedCells[up] = true
+						if randi_range(1,0) ==1:
+							if tilemap.get_cell_source_id(left)==-1 and not ProccesedCells.has(cell) and not ProccesedCells.has(left):
+								tilemap.set_cell(cell,-1)
+								tilemap.set_cell(left, ID,Vector2i(0, 0))
+								ProccesedCells[cell] = true
+								ProccesedCells[left] = true
+							elif DensityList[tilemap.get_cell_source_id(left)-1]!=null:
+								if DensityList[tilemap.get_cell_source_id(left)-1]<density  and not ProccesedCells.has(cell) and not ProccesedCells.has(right):
+									tilemap.set_cell(cell,tilemap.get_cell_source_id(left),Vector2i(0, 0))
+									tilemap.set_cell(left, ID,Vector2i(0, 0))
+									ProccesedCells[cell] = true
+									ProccesedCells[left] = true
+						else:
+							if tilemap.get_cell_source_id(right)==-1 and not ProccesedCells.has(cell) and not ProccesedCells.has(right):
+								tilemap.set_cell(cell,-1)
+								tilemap.set_cell(right, ID,Vector2i(0, 0))
+								ProccesedCells[cell] = true
+								ProccesedCells[right] = true
+							elif DensityList[tilemap.get_cell_source_id(right)-1]!=null:
+								if DensityList[tilemap.get_cell_source_id(right)-1]<density  and not ProccesedCells.has(cell) and not ProccesedCells.has(right):
+									tilemap.set_cell(cell,tilemap.get_cell_source_id(right),Vector2i(0, 0))
+									tilemap.set_cell(right, ID,Vector2i(0, 0))
+									ProccesedCells[cell] = true
+									ProccesedCells[right] = true
+		
+		
